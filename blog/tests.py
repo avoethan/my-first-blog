3f9c6123578f472f652e81c86f4be6befb44cd4e
Nebin Joshua <nebinjoshua@Nebins-MacBook-Pro.local>
2023-04-25 01:00:45 -0500

    Updated tests

--- blog/tests.py
+++ blog/tests.py
@@ -1,38 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
-"""
-from django.urls import reverse
-
-from django.contrib.auth.models import User
 
 
-class LoginTests(TestCase):
-
-    def setUp(self):
-        self.user = User.objects.create_user(
-            username='testuser', password='testpass')
-        self.url = reverse('login')
-        self.bad_credentials = {'username': 'testuser', 'password': 'wrongpass'}
-
-    def test_login_invalid(self):
-        response = self.client.post(self.url, self.bad_credentials, follow=True)
-        self.assertContains(response, 'Please enter a correct username and password.')
-
-
-# Test that submitting the login form with valid credentials redirects to the home page:
-class LoginTests(TestCase):
-    def setUp(self):
-        self.user = User.objects.create_user(
-            username='testuser', password='testpass')
-        self.url = reverse('login')
-        self.credentials = {'username': 'testuser', 'password': 'testpass'}
-
-    def test_login_valid(self):
-        response = self.client.post(self.url, self.credentials, follow=True)
-        self.assertRedirects(response, reverse('home'))
-"""
-# Test that sumbitting the login form with valid credentials redirects to the home page:
 from django.urls import reverse
 
 from django.contrib.auth.models import User
@@ -45,30 +15,16 @@ class LoginTests(TestCase):
         self.url = reverse('login')
         self.credentials = {'username': 'testuser', 'password': 'testpass'}
 
+    # Test that submitting the login form with valid credentials redirects to the home page:
     def test_login_valid(self):
         response = self.client.post(self.url, self.credentials, follow=True)
         self.assertRedirects(response, reverse('home'))
 
-# Test that submitting the login form with invalid credentials shows the correct error message:
-#from django.urls import reverse
-
-#from django.contrib.auth.models import User
-
-
-
-class LoginTests(TestCase):
-    def setUp(self):
-        self.user = User.objects.create_user(
-            username='testuser', password='testpass')
-        self.url = reverse('login')
-        self.bad_credentials = {'username': 'testuser', 'password': 'wrongpass'}
-
+    # Test that submitting the login form with invalid credentials shows the correct error message:
     def test_login_invalid(self):
         response = self.client.post(self.url, self.bad_credentials, follow=True)
         self.assertContains(response, 'Please enter a correct username and password.')
 
-# Test that the sign up page loads correctly:
-#from django.urls import reverse
 
 from django.contrib.auth.models import User
 
@@ -78,7 +34,11 @@ class SignupTests(TestCase):
         self.url = reverse('signup')
         self.credentials = {'username': 'testuser', 'password1': 'testpass', 'password2': 'testpass'}
 
+    # Test that the sign up page loads correctly:
     def test_signup_page_loads_correctly(self):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'registration/signup.html')
+
+
+self.assertTemplateUsed(response, 'registration/signup.html')
--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.landing, name='landing'),
     path('home', views.home, name='home'),
     path('sign_up', views.sign_up, name='sign_up'),
+    path('your_info', views.your_info, name='your_info'),
 ]
