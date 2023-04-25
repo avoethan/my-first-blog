48010c2bb85e31b0639d3aeeb8dca31fc225802f
Dan Mathai <danmathai23@gmail.com>
2023-04-24 22:28:53 -0500

    Created Questionaire

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.landing, name='landing'),
     path('home', views.home, name='home'),
+    path('your_info', views.your_info, name='your_info'),
     path('sign_up', views.sign_up, name='sign_up'),
 ]
