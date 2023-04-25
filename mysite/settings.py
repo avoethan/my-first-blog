96097299b9d3edf45e13f987dd46e470b8187abd
avoethan <avoethan@gmail.com>
2023-04-25 16:58:07 +0000

    new commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -138,3 +138,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/home'
 LOGOUT_REDIRECT_URL = '/login'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = Path(BASE_DIR / 'media')
